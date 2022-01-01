7e4745b38cda54e64205548b23a4cf70b313e7b6
shivansh payak <75551006+151616@users.noreply.github.com>
2022-01-01 15:43:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function trophies(){
+    location = "trophies.html";
+}
+function bed(){
+location = "bed.html";
+}
+function fish_aquarium(){
+location = "fish-aquarium.html";
+}
+function dining_table(){
+location = "dining-table.html";
+}
+function pictures(){
+location = "pictures.html";
+}
+function tea(){
+location = "tea.html";
+}
